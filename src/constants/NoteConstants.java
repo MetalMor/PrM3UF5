2a6f5377f49a0366ea41df88d15d85151c0e0a03
jogafe1415daw2 <jogafe1415daw2@gmail.com>
2016-03-02 19:50:36 +0100

    Pràctica M3UF5 Programació II // Llibreries de classes fonamentals (estable 3, doc)

---
+++ src/constants/NoteConstants.java
@@ -0,0 +1,96 @@
+package constants;
+
+/**
+ * Classe per agrupar les constants útils per al funcionament del el identificador de notas.
+ *
+ * @author Edgar Ben
+ */
+public class NoteConstants {
+
+    /**
+     * Nota Do
+     */
+    public static final String DO_NUMB = "1";
+
+    public static final String DO_STR = "Do";
+
+    /**
+     * Nota Re
+     */
+    public static final String RE_NUMB = "2";
+
+    public static final String RE_STR = "Re";
+
+    /**
+     * Nota Mi
+     */
+    public static final String MI_NUMB = "3";
+
+    public static final String MI_STR = "Mi";
+
+    /**
+     * Nota FA
+     */
+    public static final String FA_NUMB = "4";
+
+    public static final String FA_STR = "Fa";
+
+    /**
+     * Nota Sol
+     */
+    public static final String SOL_NUMB = "5";
+
+    public static final String SOL_STR = "Sol";
+
+    /**
+     * Nota La
+     */
+    public static final String LA_NUMB = "6";
+
+    public static final String LA_STR = "La";
+
+    /**
+     * Nota Si
+     */
+    public static final String Si_NUMB = "7";
+
+    public static final String Si_STR = "Si";
+
+    /**
+     * Nota DoMayor
+     */
+    public static final String DO2_NUMB = "8";
+
+    public static final String DO2_STR = "Do 2";
+
+
+    /**
+     * Nota Si Menor
+     */
+    public static final String Si2_NUMB = "9";
+
+    public static final String Si2_STR = "Si 2";
+
+    /**
+     * Nota La Menor
+     */
+    public static final String LA2_NUMB = "10";
+
+    public static final String LA2_STR = "La 2";
+
+    /**
+     * Nota Sol Menor
+     */
+    public static final String SOL2_NUMB = "11";
+
+    public static final String SOL2_STR = "Sol 2";
+    
+     /**
+     * Nota Sol Menor
+     */
+    public static final String FA2_NUMB = "12";
+
+    public static final String FA2_STR = "Fa 2";
+
+
+}
